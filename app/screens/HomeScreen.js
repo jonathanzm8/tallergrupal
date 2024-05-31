14a35fe6973ed740e31021341cafbeb91eac01a1
jonathanzm8 <jzhina.done@gmail.com>
2024-05-30 22:43:21 -0500

    cambiando colores y  texto

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -2,7 +2,8 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 
 export const Home = ({ navigation }) => {
     return <View style={styles.container}>
-        <Text>HOME</Text>
+        <Text>Bienvenido a mi aplicacion , soy Jonathan Zhiña</Text>
+        <Text>resolviendo el taller </Text>
 
         <View style={styles.botones}>
 
@@ -33,7 +34,7 @@ export const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#00ffff',
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -47,6 +48,7 @@ const styles = StyleSheet.create({
     boton1: {
 
         marginRight: 60,
+        backgroundColor: 'green'
 
     },
 });
